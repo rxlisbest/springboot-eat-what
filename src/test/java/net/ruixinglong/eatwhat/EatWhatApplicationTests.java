fedacc36f4b3b968d49360b7d301e37298f2ddc5
Roy <rxlisbest@gmail.com>
2019-04-03 14:25:11 +0800

    project init

---
+++ src/test/java/net/ruixinglong/eatwhat/EatWhatApplicationTests.java
@@ -0,0 +1,16 @@
+package net.ruixinglong.eatwhat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EatWhatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
